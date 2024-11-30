d1f5d89c07968de93b7d2d3d9b873d5465e9695a
Abaiev-M <abaievmisha@gmail.com>
2024-11-30 16:23:27 +0200

    added uiLabel

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textLabel.text = "Hello, World!"
         // Do any additional setup after loading the view.
     }
 
