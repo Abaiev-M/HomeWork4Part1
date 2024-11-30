3d0de9f337baef3a36a26fcfd6523bc3c4d195c9
Abaiev-M <abaievmisha@gmail.com>
2024-11-30 13:05:03 +0200

    added an empty project

---
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork4Part1
+//
+//  Created by Misha Abaiev on 30.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
